816b40ea4da28040ebc2120ecbd599288d8c1109
umyomyomyon <minecraft.sentry.yym@gmail.com>
2023-04-09 02:59:43 +0900

    add typing animation

--- src/app/components/HeaderText.tsx
+++ src/app/components/HeaderText.tsx
@@ -1,10 +1,11 @@
 import { FC } from 'react';
 import { Colon, Equal, Brace } from './symbols';
 import { Caret } from './Caret';
+import styles from './text.module.css';
 
 export const HeaderText: FC = () => {
   return (
-    <h1 className="text-dynamic caret-transparent my-4">
+    <h1 className={styles.text}>
       <span className="text-const">const</span> <span className="text-variable">umyomyomyon</span>
       <span className="hidden md:inline">
         <Colon /> <span className="text-type">WhoAmI</span>
