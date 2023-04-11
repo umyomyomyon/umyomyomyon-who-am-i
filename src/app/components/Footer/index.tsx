e9fbbe32f8f50472dbe47a8d236c1e62858d3672
umyomyomyon <minecraft.sentry.yym@gmail.com>
2023-04-12 01:44:13 +0900

    fix brace pos

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import { Brace } from '../symbols';
 export const Footer: FC = () => {
   return (
     <footer className="grid place-content-center pb-32">
-      <p className="text-header caret-transparent my-4">
+      <p className="text-header caret-transparent my-4 w-[21ch] md:w-[29ch]">
         <Brace right />
       </p>
     </footer>
