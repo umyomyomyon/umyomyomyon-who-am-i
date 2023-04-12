757177ee3711cd2605ff44f8fa2c5459cc2f8207
umyomyomyon <minecraft.sentry.yym@gmail.com>
2023-04-13 02:24:00 +0900

    fix brace pos

--- src/app/components/Content/index.tsx
+++ src/app/components/Content/index.tsx
@@ -1,12 +1,18 @@
 import { FC } from 'react';
 import { Fields } from '../Fields';
 import { Icon } from '../Icon';
+import { Brace } from '../symbols';
 
 export const Content: FC = () => {
   return (
     <main className="flex min-h-[50vh] flex-col items-center">
       <Icon />
-      <Fields />
+      <section className='relative'>
+        <Fields />
+        <div className='text-header caret-transparent w-full absolute left-[-10%] md:left-[-20%]'>
+          <Brace right />
+        </div>
+      </section>
     </main>
   );
 };
--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,12 +1,8 @@
 import { FC } from 'react';
-import { Brace } from '../symbols';
 
 export const Footer: FC = () => {
   return (
     <footer className="grid place-content-center pb-32">
-      <p className="text-header caret-transparent my-4 w-[21ch] md:w-[29ch]">
-        <Brace right />
-      </p>
     </footer>
   );
 };
