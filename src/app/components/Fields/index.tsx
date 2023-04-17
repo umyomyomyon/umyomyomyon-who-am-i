e5213d9e6ada182af43cc73a3bb9133818c9c7b1
umyomyomyon <minecraft.sentry.yym@gmail.com>
2023-04-17 23:13:37 +0900

    このサイトのリポジトリへのリンクを追加

--- src/app/components/Fields/index.tsx
+++ src/app/components/Fields/index.tsx
@@ -27,6 +27,16 @@ export const Fields: FC = () => {
         }
       />
       <KV property="yearsOfExperience" value={<span className="text-number">3</span>} />
+      <KV
+        property="src"
+        value={
+          <span>
+            <a href="https://github.com/umyomyomyon/umyomyomyon-who-am-i" className="z-10">
+              <img src="/github-mark.svg" width={35} className="inline" />
+            </a>
+          </span>
+        }
+      />
       <Skills />
     </ul>
   );
