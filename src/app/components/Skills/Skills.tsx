78d2e503d2da9b4b0186f6a8ea7fb83de8c35e63
umyomyomyon <minecraft.sentry.yym@gmail.com>
2023-04-17 22:37:27 +0900

    skills修正

--- src/app/components/Skills/Skills.tsx
+++ src/app/components/Skills/Skills.tsx
@@ -1,59 +1,16 @@
-import { FC, useState } from 'react';
-import { Comma, Colon } from '../symbols';
+import { FC } from 'react';
+import { Colon, Bracket } from '../symbols';
 import { SkillsContent } from './Content';
 
 const skills = ['TypeScript', 'React', 'Redux', 'Next.js', 'Python', 'Django', 'GCP', 'Firebase'];
 
 export const Skills: FC = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const handleClick = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <li className="my-5 caret-transparent">
-      <details>
-        <summary className="list-none cursor-pointer" onClick={handleClick}>
-          <span className="text-property">skills</span>
-          <Colon /> <SkillsValue isOpen={isOpen} />
-        </summary>
-        <>
-          <SkillsContent skills={skills} />
-          <span className="text-brackets">]</span>
-        </>
-      </details>
+      <span className="text-property">skills</span>
+      <Colon /> <Bracket />
+      <SkillsContent skills={skills} />
+      <Bracket right />
     </li>
   );
-};
-
-type ValueProps = {
-  isOpen: boolean;
-};
-
-const SkillsValue: FC<ValueProps> = ({ isOpen }) => {
-  return (
-    <>
-      {isOpen ? (
-        <OpenSkills />
-      ) : (
-        <>
-          <ClosedSkills />
-          <Comma />
-        </>
-      )}
-    </>
-  );
-};
-
-const ClosedSkills: FC = () => {
-  return (
-    <>
-      <span className="text-brackets">[</span> <span className="text-other">...</span>{' '}
-      <span className="text-brackets">]</span>
-    </>
-  );
-};
-
-const OpenSkills: FC = () => {
-  return <span className="text-brackets">[</span>;
 };
